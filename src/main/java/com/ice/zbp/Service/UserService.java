1ab1a01b516ccedc6be28d68b54ae367525f1966
iceZhang <zhangsun1987@gmail.com>
2014-08-15 14:10:21 +0800

    test

---
+++ src/main/java/com/ice/zbp/Service/UserService.java
@@ -0,0 +1,9 @@
+package com.ice.zbp.Service;
+
+/**
+ * Created by ice on 2014-08-15.
+ */
+public interface UserService {
+
+
+}
---
+++ src/main/java/com/ice/zbp/Service/impl/UserServiceImpl.java
@@ -0,0 +1,9 @@
+package com.ice.zbp.Service.impl;
+
+/**
+ * Created by ice on 2014-08-15.
+ */
+public class UserServiceImpl {
+
+
+}
