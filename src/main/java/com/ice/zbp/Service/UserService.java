43a4c3a26347a0153f11116df538fdbb4ba58862
iceZhang <zhangsun1987@gmail.com>
2014-08-15 14:47:21 +0800

    delete

--- src/main/java/com/ice/zbp/Service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.ice.zbp.Service;
-
-/**
- * Created by ice on 2014-08-15.
- */
-public interface UserService {
-
-
-}
--- src/main/java/com/ice/zbp/Service/impl/UserServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.ice.zbp.Service.impl;
-
-/**
- * Created by ice on 2014-08-15.
- */
-public class UserServiceImpl {
-
-
-}
